5c54201fa28b7d9fbe8dfd5ec7cc1537e43fbfc2
Kadi Bibas <kadibibas2gmail.com>
2017-05-09 21:40:35 +0300

    אודות

--- app/src/main/java/com/example/kadibibas/limodim/kesher.java
+++ app/src/main/java/com/example/kadibibas/limodim/kesher.java
@@ -1,19 +1,21 @@
 package com.example.kadibibas.limodim;
 
-import android.app.Activity;
+import android.Manifest;
 import android.content.Intent;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
-import android.view.MenuItem;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
 import android.view.View;
-import android.view.Window;
 import android.widget.Button;
 
-public class kesher extends  AppCompatActivity{
 
+public class kesher extends AppCompatActivity {
+
+    private static final int MY_PERMISSIONS_REQUEST_CALL_PHONE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +23,36 @@ public class kesher extends  AppCompatActivity{
         setContentView(R.layout.activity_kesher);
 
         final Button button2 = (Button) findViewById(R.id.button2);
+        final Button kadi = (Button) findViewById(R.id.kadi);
+
+
+        final Button Phone = (Button) findViewById(R.id.imageButton2);
+        Phone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent sIntent = new Intent(Intent.ACTION_CALL);
+                sIntent.setData(Uri.parse("tel:0545377335"));
+                if (ActivityCompat.checkSelfPermission(kesher.this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return;
+                }
+                startActivity(sIntent);
+
+            }
+            });
+
+
+
+
+        kadi.setText(Html.fromHtml("<a href=\"mailto:kadibibas@gmail.com\">.</a>"));
+        kadi.setMovementMethod(LinkMovementMethod.getInstance());
+
 
         button2.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -31,14 +63,6 @@ public class kesher extends  AppCompatActivity{
         });
 
 
-        /*Toolbar my_Toolbar = (Toolbar) findViewById(R.id.mCustomToolbar);
-        setSupportActionBar(my_Toolbar);
-
-        setSupportActionBar(my_Toolbar);
-
-        getSupportActionBar().setTitle("");
-        getSupportActionBar();
-        */
     }
 
 }
