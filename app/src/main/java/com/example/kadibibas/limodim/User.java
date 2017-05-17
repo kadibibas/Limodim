362730d5215a3693cb3092c7d83369a31accabda
Ishai Levi <levishai87@gmail.com>
2017-05-17 17:40:30 +0300

    דף פרופיל משתמש

--- app/src/main/java/com/example/kadibibas/limodim/User.java
+++ app/src/main/java/com/example/kadibibas/limodim/User.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class User extends Activity {
@@ -15,6 +16,18 @@ public class User extends Activity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_user);
 
+        ImageView Profilimage=(ImageView) findViewById(R.id.imageView4);
+        Profilimage.setImageResource(R.drawable.ishai);
+
+        ImageView mail=(ImageView) findViewById(R.id.imageView5);
+        mail.setImageResource(R.drawable.ic_email_black_24dp);
+
+        ImageView tel=(ImageView) findViewById(R.id.imageView6);
+        tel.setImageResource(R.drawable.telephone);
+
+        ImageView stars=(ImageView) findViewById(R.id.imageView8);
+        stars.setImageResource(R.drawable.rating2);
+/*
         final EditText etname = (EditText) findViewById(R.id.etname);
         final EditText etphone = (EditText) findViewById(R.id.etphone);
         final EditText etAddress = (EditText) findViewById(R.id.etAddress);
@@ -23,6 +36,7 @@ public class User extends Activity {
         final TextView tvphone = (TextView) findViewById(R.id.tvphone);
         final TextView tvProfession = (TextView) findViewById(R.id.tvProfession);
         final TextView tvAddress = (TextView) findViewById(R.id.tvAddress);
+*/
 
 
     }
